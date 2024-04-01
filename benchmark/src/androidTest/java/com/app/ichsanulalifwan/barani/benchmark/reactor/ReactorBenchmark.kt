822afeb62ed549f4dfaf3aa56e787a538984f652
ichsanul.alifwan <ichsanulalifwan@gmail.com>
2024-04-02 06:44:35 +0800

    update ReactorBenchmark impl

--- benchmark/src/androidTest/java/com/app/ichsanulalifwan/barani/benchmark/reactor/ReactorBenchmark.kt
+++ benchmark/src/androidTest/java/com/app/ichsanulalifwan/barani/benchmark/reactor/ReactorBenchmark.kt
@@ -1,19 +1,19 @@
-package com.app.ichsanulalifwan.barani.benchmark.rxjava
+package com.app.ichsanulalifwan.barani.benchmark.reactor
 
 import android.content.Context
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.test.platform.app.InstrumentationRegistry
-import com.app.ichsanulalifwan.barani.core.data.repository.news.rxjava.RxJavaNewsRepository
+import com.app.ichsanulalifwan.barani.core.data.repository.news.reactor.ReactorNewsRepository
 import com.app.ichsanulalifwan.barani.core.data.source.local.room.AppDatabase
 import com.app.ichsanulalifwan.barani.core.data.source.local.room.NewsDao
 import com.app.ichsanulalifwan.barani.core.data.source.remote.network.ApiConfig
-import com.app.ichsanulalifwan.barani.core.data.source.remote.network.rxjava.RxJavaNewsApiService
+import com.app.ichsanulalifwan.barani.core.data.source.remote.network.reactor.ReactorNewsApiService
 import org.junit.Rule
 
-open class RxJavaBenchmark {
+open class ReactorBenchmark {
 
-    protected val remoteDataSource: RxJavaNewsApiService by lazy {
-        ApiConfig.getMockApiService(context = context)
+    protected val remoteDataSource: ReactorNewsApiService by lazy {
+        ApiConfig.getMockBenchmarkApiService(context = context)
     }
 
     protected val localDataSource: NewsDao by lazy {
@@ -24,7 +24,7 @@ open class RxJavaBenchmark {
     val benchmarkRule = BenchmarkRule()
 
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
-    protected val repository = RxJavaNewsRepository(
+    protected val repository = ReactorNewsRepository(
         remoteDataSource = remoteDataSource,
         localDataSource = localDataSource,
     )
